9e588a4ab0efc14cea867692f791530e28234c17
ArionDutta33 <ariondutta333@outlook.com>
2024-04-17 14:51:33 +0530

    Added the animations to the page

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ t.from(".nav .logo", {
     y: -100,
     duration: .8
 })
+t.from(".header #one", {
+    opacity: 0,
+    x: -100,
+
+    duration: .6
+})
+t.from(".header #two", {
+    y: -100,
+    opacity: 0,
+    duration: .6
+})
+t.from(".header #three", {
+    x: 100,
+    opacity: 0,
+    duration: .6
+})
+t.from(".main .main-btn", {
+    opacity: 0,
+    scale: 1.1,
+    rotate: 360,
+    duration: .8
+})
